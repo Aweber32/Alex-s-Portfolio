da5aa8949d255efad24ddb1490f471fdf1d1894c
Alex Weber <alexweber12899@gmail.com>
2022-02-23 16:32:17 -0500

    Addition of Languages

--- app.py
+++ app.py
@@ -16,9 +16,33 @@ def humana():
 def untitled():
     return render_template('Untitled.html')
 
+#Tech Writing 
 @app.route('/System_Analysis_Design')
 def sys_analysis_des():
     return render_template('sys_analysis_des.html')
+
+@app.route('/Visio_Database_Design')
+def visio_databaseD():
+    return render_template('visio_databaseD.html')
+
+@app.route('/Case_WriteUps')
+def case_writeups():
+    return render_template('case_writeups.html')
+
+#Languages / Software
+@app.route('/Front_end_languages')
+def front_end():
+    return render_template('front_end.html')
+
+@app.route('/Back_end_languages')
+def back_end():
+    return render_template('back_end.html')
+
+@app.route('/PowerBI')
+def powerBI():
+    return render_template('powerBI.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
